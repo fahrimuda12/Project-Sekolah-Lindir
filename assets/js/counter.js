4111fc8786842df42b71923d2ee7ffd9e5799a62
fahrimuda12 <profesorrob01@gmail.com>
2021-01-02 08:06:40 +0700

    Menambahkan shopping cart

--- assets/js/counter.js
+++ assets/js/counter.js
@@ -26,31 +26,31 @@ let product = [
     {
         name:'Lixy 3',
         tag:'lixy1',
-        price:'1400000',
+        price:1400000,
         inCart: 0
     },
     {
         name:'Lixy 2',
         tag:'lixy2',
-        price:'800000',
+        price:800000,
         inCart: 0
     },
     {
         name:'Nalog',
         tag:'nalog',
-        price:'730000',
+        price:730000,
         inCart: 0
     },
     {
-        name:'Minmal',
-        tag:'minmal',
-        price:'300000',
+        name:'Simpli',
+        tag:'simpli',
+        price:300000,
         inCart: 0
     },
     {
         name:'Lixy 1',
-        tag:'lixy',
-        price:'250000',
+        tag:'lixy1',
+        price:250000,
         inCart: 0
     }
 
@@ -58,6 +58,7 @@ let product = [
 for(let i=0; i < buttonMe.length; i++){
     buttonMe[i].addEventListener('click',() => {
         nambahNotif(product[i]);
+        totalCost(product[i]);
     })
 }
 
@@ -106,5 +107,72 @@ function setItems(product){
 
     localStorage.setItem('productInCart',JSON.stringify(cartItems));
 }
+function totalCost(product){
+    //console.log("the product is ",product.price);
+
+    let cartCost = localStorage.getItem('totalCost');
+    console.log(typeof cartCost);
+
+    if(cartCost != null){
+        cartCost = parseInt(cartCost); 
+        localStorage.setItem('totalCost',cartCost + product.price);
+    }
+    else{
+        localStorage.setItem('totalCost', product.price);
+    }
+    
+
+}
+
+function displayCart(){
+    let cartCost = localStorage.getItem('totalCost');
+    let cartItems = localStorage.getItem('productInCart');
+    cartItems = JSON.parse(cartItems);
+    //console.log(cartItemS);
+    let selectContainer = document.querySelector('.produk');
+    if(cartItems && selectContainer ){
+        selectContainer.innerHTML ='';
+        Object.values(cartItems).map(item =>{
+            selectContainer.innerHTML += `
+            <div class="row row-cols-4 text-uppercase text-counter produk d-flex align-items-center">
+                <div class="col-md-5 col-sm-2">
+                    <i  class="far fa-times-circle text-counter"></i>
+                    <img src="../assets/img/${item.tag}.jpg" class="card-img-fluid img-card-counter p-1" height="150px" width="150px" />
+                    <span>${item.name}</span>
+                </div>
+                <div class="col-md-3 col-sm-3">
+                    <span class="text-counter">${item.price}</span>
+                </div>
+                <div class="col-md-2 col-sm-3 text-center">
+                    <i class="fas fa-minus text-primary pr-md-2"></i>
+                    <span class="text-counter">${item.inCart}</span>
+                    <a class="button-select" type="button"><i class="fas fa-plus pl-md-2 text-primary"></i></a>
+                </div>
+                <div class="col-md-2 col-sm-4">
+                    <span class="text-counter">${item.inCart * item.price}</span>
+                </div>
+            </div>
+            `
+            
+        });
+        
+        selectContainer.innerHTML += `
+            <div class="row text-uppercase text-counter justify-content-end">
+                <div class="col-2 text-center">
+                    <h4>Total</h4>
+                </div>
+                <div class="col-2">
+                    <h4 >Rp. ${cartCost}</h4>
+                </div>
+            </div>
+            <div class="row text-uppercase text-counter justify-content-end my-3">
+                <div class="col-2 mb-3">
+                    <a class="btn btn-outline-primary px-4 rounded-pill" href="#" role="button">Buy</a>
+                </div>
+            </div>
+            `
+    }
+}   
 onLoadNotif();
+displayCart();
 console.log('page is fully loaded');
